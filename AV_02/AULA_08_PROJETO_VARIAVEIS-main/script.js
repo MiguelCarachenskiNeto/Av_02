2cc9cc7593e6360f60b96e9e2f96692ca6596c48
MiguelCarachenskiNeto <116725585+MiguelCarachenskiNeto@users.noreply.github.com>
2024-11-25 09:37:41 -0300

    Add files via upload

---
+++ AV_02/AULA_08_PROJETO_VARIAVEIS-main/script.js
@@ -0,0 +1,25 @@
+var nome = "Jefferson";
+console.log(nome);
+
+if (nome == "Jefferson") {
+  var escola = "CESF";
+  console.log(escola);
+}
+
+console.log(escola);
+escola = "sagrada";
+console.log(escola);
+
+
+if (nome === "Jefferson") {
+  let curso = "ds";
+  console.log(curso);
+}
+//console.log(curso);
+curso = "culinaria";
+console.log(curso);
+
+const cargo = "estudante";
+console.log(cargo);
+cargo = "estagiario";
+console.log(cargo);
---
+++ AV_02/AULA_09_PROJETO_OBJETO-main/script.js
@@ -0,0 +1,31 @@
+let pessoa = {
+    nome: "Miguel",
+    sobrenome: "Carachenski",
+    idade: 17,
+    altura: 1.65,
+    cargo: "Estudante",
+  };
+  
+  console.log(pessoa);
+  
+  console.log(pessoa.nome);
+  console.log(pessoa.altura);
+  
+  let carro = {
+    nome: "Brasilia",
+    cor: "Amarelo",
+    potencia: "80cv",
+  };
+  
+  console.log(carro.nome + " " + carro.cor);
+  
+  let usuarios = [
+    { nome: "Albertina", cargo: "programador", status: "ativo" },
+    { nome: "Bilbonei", cargo: "Engenheiro de software", status: "ativo" },
+    { nome: "Codegunde", cargo: "Estagiario", status: "inativo" },
+  ];
+  
+  console.log(usuarios);
+  
+  console.log(usuarios[2]);
+  console.log(usuarios[2].nome);
